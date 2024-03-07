6e1de81c9f311ffcb5e81c9fb69d70eb38ae66db
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-03-07 13:17:30 -0600

    callback function  is called on change

--- __tests__/SkillSelect.test.js
+++ __tests__/SkillSelect.test.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { cleanup, render } from '@testing-library/react'
+import { cleanup, fireEvent, render } from '@testing-library/react'
 import { act } from 'react-test-renderer'
 import SkillSelect from '../src/components/SkillSelect/SkillSelect'
 
+const fakeSkills = [
+    {
+        "id": 1,
+        "skill": "skill 1"
+    },
+    {
+        "id": 2,
+        "skill": "skill 2"
+    },
+    {
+        "id": 3,
+        "skill": "skill 3"
+    }
+]
 
 beforeEach(
     async () =>
@@ -26,15 +40,18 @@ const testRender = async () =>
     await act(
         async () =>
         {
-            returnRender = await render(<SkillSelect />)
+            returnRender = await render(<SkillSelect skills={fakeSkills} onSkillSelected={onSkillSelected} />)
         }
     )
     return returnRender
 }
 
+const onSkillSelected = jest.fn()
+
 describe('SkillSelect component works',
     () =>
     {
+
         it('renders',
             async () =>
             {
@@ -43,5 +60,33 @@ describe('SkillSelect component works',
                 expect(tree.container).toBeInTheDocument()
             }
         )
+
+        it('makes an option for each skill passed',
+            async () =>
+            {
+                const tree = await testRender()
+
+                const options = tree.getAllByRole('option')
+
+                for(const skill of fakeSkills) 
+                {
+                    const thisOption = options.find(option => option.key = skill.id)
+                    expect(thisOption).toBeInTheDocument()
+                }
+            }
+        )
+
+        it('calls callback function, passing value, on skill selected',
+            async () =>
+            {
+                const tree = await testRender()
+
+                const select = tree.getByRole('combobox')
+                fireEvent.change(select, {target: {value: "test value"}})
+
+                expect(onSkillSelected).toHaveBeenCalledWith("test value")
+            }
+        )
+
     }
 )
--- src/components/SkillSelect/SkillSelect.jsx
+++ src/components/SkillSelect/SkillSelect.jsx
@@ -1,7 +1,23 @@
 
-const SkillSelect = () =>
+const SkillSelect = ({ skills, onSkillSelected }) =>
 {
+    const onSelectChanged = (event) =>
+    {
+        onSkillSelected(event.target.value)
+    }
 
+    return <div>
+        <select onChange={onSelectChanged}>
+            {
+                skills.map(
+                    (skill) =>
+                    {
+                        return <option value={skill.id} key={skill.id}>{skill.skill}</option>
+                    }
+                )
+            }
+        </select>
+    </div>
 }
 
 export default SkillSelect
