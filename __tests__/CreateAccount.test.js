9baeec69eab835b76764e96d05118e2ceb3af099
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-03-14 10:35:11 -0500

    wrote tests for create account page and made layout for html

--- __tests__/CreateAccount.test.js
+++ __tests__/CreateAccount.test.js
@@ -2,9 +2,12 @@ import React from 'react'
 import '@testing-library/jest-dom'
 import CreateAccount from '../src/pages/CreateAccount'
 import { Router, Routes } from 'react-router-dom'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import { createMemoryHistory } from 'history';
 import { act } from 'react-test-renderer'
+
+jest.mock('../src/services/userService')
+import { createAccount } from '../src/services/userService'
 
 let currentUser = { id: 1, username: "lincolnpepper" }
 
@@ -41,6 +44,92 @@ describe('CreateAccount works',
             }
         )
 
+        it('creates all interactables',
+            async () =>
+            {
+                const tree = await testRender()
+
+                const usernameField = tree.getByRole('textbox')
+                const createAccountButton = tree.getByRole('button', { name: /create account/i })
+                const logInButton = tree.getByRole('button', { name: /log in/i })
+                expect(usernameField).toBeInTheDocument()
+                expect(createAccountButton).toBeInTheDocument()
+                expect(logInButton).toBeInTheDocument()
+            }
+        )
+
+        it('creates an account when create account button is pressed and username field is valid, then navigates to log in page',
+            async () =>
+            {
+                const mockNavigate = jest.fn()
+                jest.spyOn(React, 'useNavigate').mockImplementation(() => mockNavigate)
+
+                const testUser = { username: "test username" }
+
+                const tree = await testRender()
+
+                const usernameField = tree.getByRole('textbox')
+                const createAccountButton = tree.getByRole('button', { name: /create account/i })
+
+                await act(
+                    async () =>
+                    {
+                        await fireEvent.change(usernameField, { target: { value: testUser.username } })
+                    }
+                )
+
+                await act(
+                    async () =>
+                    {
+                        await fireEvent.click(createAccountButton)
+                    }
+                )
+
+                expect(createAccount).toHaveBeenCalledWith(testUser)
+                expect(mockNavigate).toHaveBeenCalledWith("/login")
+            }
+        )
+
+        it('calls window alert when create account is pressed but username is not valid',
+            async () =>
+            {
+                window.alert = jest.fn()
+
+                const tree = await testRender()
+
+                const createAccountButton = tree.getByRole('button', { name: /create account/i })
+
+                await act(
+                    async () =>
+                    {
+                        await fireEvent.click(createAccountButton)
+                    }
+                )
+
+                expect(window.alert).toHaveBeenCalled()
+            }
+        )
+
+        it('calls navigate, passing correct url',
+            async () =>
+            {
+                const mockNavigate = jest.fn()
+                jest.spyOn(React, 'useNavigate').mockImplementation(() => mockNavigate)
+
+                const tree = await testRender()
+
+                const logInButton = tree.getByRole('button', { name: /log in/i })
+
+                await act(
+                    async () =>
+                    {
+                        await fireEvent.click(logInButton)
+                    }
+                )
+
+                expect(mockNavigate).toHaveBeenCalledWith("/login")
+            }
+        )
 
     }
 )
--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,7 +1,29 @@
 
 const CreateAccount = () =>
 {
-    
+
+    const onUsernameInputChanged = () =>
+    {
+
+    }
+
+    const onLoginButtonClicked = () =>
+    {
+        
+    }
+
+    return <div>
+        <h1 className="text-6xl">Create Account</h1>
+        <div className="space-x-5">
+            <label name="username">username:</label>
+            <input name="username" type="text" className="text-black" onChange={onUsernameInputChanged} />
+            <button onClick={onLoginButtonClicked}>Create Account</button>
+        </div>
+        <div className="flex">
+            <p>Have an account?  </p>
+            <button>Log In</button>
+        </div>
+    </div>
 }
 
 export default CreateAccount
