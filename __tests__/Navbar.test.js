b805836cf72410a25af2316773ed795ab55ec9e3
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-03-19 13:55:15 -0500

    updated navbar tests

--- __tests__/Navbar.test.js
+++ __tests__/Navbar.test.js
@@ -15,6 +15,9 @@ describe('Navbar should navigate to associated pages when buttons are pressed',
                         <Route path="/">
                             <Route index element={<Navbar />} />
                             <Route path="/exercises" element={<div data-testid="exercise-page"></div>} />
+                            <Route path="/playlist" element={<div data-testid="playlist-page" />} />
+                            <Route path="/practice" element={<div data-testid="practice-page" />} />
+                            <Route path="/login" element={<div data-testid="login-page" />} />
                         </Route>
                     </Routes>
                 </MemoryRouter>
@@ -28,10 +31,50 @@ describe('Navbar should navigate to associated pages when buttons are pressed',
             {
                 const tree = testRender()
 
-                const exerciseButton = tree.getByText('Exercises')
+                const exerciseButton = tree.getByText(/exercise/i)
                 fireEvent.click(exerciseButton)
 
                 expect(tree.getByTestId("exercise-page")).toBeInTheDocument()
+            }
+        )
+
+        it('navigates to playlist when playlist is clicked',
+            () =>
+            {
+                const tree = testRender()
+
+                const playlistButton = tree.getByText(/playlist/i)
+                fireEvent.click(playlistButton)
+
+                expect(tree.getByTestId('playlist-page')).toBeInTheDocument()
+            }
+        )
+
+        it('navigates to practice when practice is clicked',
+            () =>
+            {
+                const tree = testRender()
+
+                const playlistButton = tree.getByText(/practice/i)
+                fireEvent.click(playlistButton)
+
+                expect(tree.getByTestId('practice-page')).toBeInTheDocument()
+            }
+        )
+
+        it('navigates to login when logout is clicked, and logs out',
+            () =>
+            {
+                const spy = jest.spyOn(Object.getPrototypeOf(localStorage), 'removeItem')
+                Object.setPrototypeOf(localStorage.removeItem, jest.fn())
+
+                const tree = testRender()
+
+                const playlistButton = tree.getByText(/logout/i)
+                fireEvent.click(playlistButton)
+
+                expect(tree.getByTestId('login-page')).toBeInTheDocument()
+                expect(spy).toHaveBeenCalledWith('guitar-practicer-user')
             }
         )
     }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () =>
 
     const onPracticeClicked = () =>
     {
-
+        navigate("/practice")
     }
 
     const onLogoutClicked = () =>
